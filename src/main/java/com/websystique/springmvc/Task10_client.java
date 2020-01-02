2c5b9046ba9f1b4c7b897be0607e28d6380a36ab
Vladimir Filippov <sergeiych@gmail.com>
2020-01-03 02:09:04 +0300

    first client

---
+++ src/main/java/com/websystique/springmvc/Task10_client.java
@@ -0,0 +1,12 @@
+package com.websystique.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task10_client {
+    public static void main(String[] args) {
+        SpringApplication.run(Task10_client.class, args);
+    }
+
+}
